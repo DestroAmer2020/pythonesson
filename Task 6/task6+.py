cc130b40440cc060951749480237d66516d17adb
DestroAmer2020 <116159391+DestroAmer2020@users.noreply.github.com>
2023-12-13 21:11:30 +0200

    Add files via upload

---
+++ Task 6/task6+.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+def logger(func):
+    logs = []
+    def wrapper(*args, **kwargs):
+        start_time = datetime.now()
+        result = func(*args, **kwargs)
+        end_time = datetime.now()
+        log_entry = {
+            'time': start_time,
+            'function': func.__name__,
+            'arguments': args,
+            'result': result,
+            'execution_time': end_time - start_time
+        }
+        logs.append(log_entry)
+        with open('log.txt', 'a') as file:
+            file.write(f"{log_entry}\n")
+        return result
+    return wrapper
+def get_logs():
+    for log_entry in logs:
+        yield log_entry
+@logger
+def multiply(a, b):
+    return a * b
+multiply(3, 4)
+multiply(5, 6)
+log = get_logs()
+print(next(log))
+print(next(log))
